3b152c13cbaf4d057a86c5bf022450eaed95f3f2
Bijae Lim <ibj1231@naver.com>
2024-11-09 18:42:06 +0900

    ㅠbefore file attach

--- src/main/java/com/works/board/controller/BoardController.java
+++ src/main/java/com/works/board/controller/BoardController.java
@@ -2,14 +2,16 @@ package com.works.board.controller;
 
 import com.works.board.entity.Board;
 import com.works.board.service.BoardService;
+import org.apache.catalina.users.SparseUserDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BoardController {
 
-    @Autowired
+    @Autowired //스프링 빈이 의존성주입 해줌
     private BoardService boardService;
 
     @GetMapping("/board/write") //localhost:8080/board/write
@@ -19,10 +21,60 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board, Model model){
 
-        boardService.write(board);
+        boardService.boardWrite(board);
 
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다."); //글작성 시 alert 띄우기용
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, @RequestParam(name = "id")  Integer id) {
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(@RequestParam(name = "id") Integer id){
+        boardService.boardDelete(id);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id,
+                              Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
+
+        Board boardTemp = boardService.boardView(id); //기존에 작성된 글
+        boardTemp.setTitle(board.getTitle());         //새로 작성한글로 덮어씌우기
+        boardTemp.setContent(board.getContent());     //새로 작성한글로 덮어씌우기
+
+        boardService.boardWrite(boardTemp); //새로 작성한 내용으로 저장
+
+        model.addAttribute("message", "수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
+//        return "redirect:/board/list"; 수정완료 alert 안쓸때는 리다이렉트로 전달
     }
 }
--- src/main/java/com/works/board/service/BoardService.java
+++ src/main/java/com/works/board/service/BoardService.java
@@ -5,14 +5,39 @@ import com.works.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class BoardService {
 
-    @Autowired
+    @Autowired //스프링 빈이 의존성주입 해줌
     private BoardRepository boardRepository;
 
-    public void write(Board board) {
+    // 글 작성 처리
+    public void boardWrite(Board board) {
 
         boardRepository.save(board);
+    }
+
+    // 게시글 리스트 처리
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();  //findById로 쓰면 옵셔널 값으로 받아와서 .get() 해줘야함
+
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+
+        boardRepository.deleteById(id);
+
     }
 }
